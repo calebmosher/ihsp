6c45912bbbbaa6551735b1d57083912f62c0fd39
Cal Mosher <cal@chevro.co>
2016-08-19 13:19:04 -0700

    Add intro and who we are sections

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 
 <body>
 	<main>
-		<div class="hero">
+		<section class="hero">
 			<nav>
 				<ul>
 					<li><a href="#">About</a></li>
@@ -34,7 +34,33 @@
 				<h1>Get your life back.</h1>
 				<a class="cta">Request an appointment</a>
 			</div>
-		</div>
+		</section>
+
+		<section class="intro">
+			<div class="wrap">
+				<span class="spinal-xray"></span>
+
+				<h2>Take your life off of hold.</h2>
+				<p class="copy">Chiropractic care means getting back to the things you love. We provide one of the widest arrays of services in the Dayton Area for holistic wellness. We'll get you back on your feet, and back to your life.</p>
+				<p class="copy"><a class="tout" href="/services">See how</a>
+			</div>
+		</section>
+
+		<section class="who-we-are-heading">
+			<div class="wrap">
+				<h2>Meet your team of specialists.</h2>
+			</div>
+		</section>
+
+		<section class="who-we-are">
+			<div class="wrap">
+				<span class="dr-curry"></span>
+
+				<h2>Dr. Nick Curry, D.C.</h2>
+				<p class="copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc volutpat varius erat non sagittis. Proin ornare tortor nec elementum suscipit. Vivamus feugiat, nunc eget interdum convallis, lectus ligula rhoncus dolor, a sodales leo eros nec sapien. Vivamus at rutrum magna. Integer quis consequat nunc.</p>
+				<p class="copy"><a class="tout" href="#">Meet the team</a></p>
+			</div>
+		</section>
 
 		<footer>&copy; 2016</footer>
 	</main>
