3ecae47c6cb9610b0218687855b5eab0c22ed139
Cal Mosher <cal@chevro.co>
2016-08-19 18:59:51 -0700

    Finish last section, create footer

--- index.php
+++ index.php
@@ -36,23 +36,23 @@
 			</div>
 		</section>
 
-		<section class="intro">
+		<section class="intro section-main">
 			<div class="wrap">
-				<span class="spinal-xray"></span>
+				<span class="woman-running"></span>
 
-				<h2>Take your life off of hold.</h2>
+				<h2>Take your days off of hold.</h2>
 				<p class="copy">Chiropractic care means getting back to the things you love. We provide one of the widest arrays of services in the Dayton Area for holistic wellness. We'll get you back on your feet, and back to your life.</p>
 				<p class="copy"><a class="tout" href="/services">See how</a>
 			</div>
 		</section>
 
-		<section class="who-we-are-heading">
+		<section class="who-we-are-heading section-heading">
 			<div class="wrap">
 				<h2>Meet your team of specialists.</h2>
 			</div>
 		</section>
 
-		<section class="who-we-are">
+		<section class="who-we-are section-main">
 			<div class="wrap">
 				<span class="dr-curry"></span>
 
@@ -62,7 +62,34 @@
 			</div>
 		</section>
 
-		<footer>&copy; 2016</footer>
+		<section class="let-us-know-heading section-heading">
+			<div class="wrap">
+				<h2>Get the specific care you need.</h2>
+			</div>
+		</section>
+
+		<section class="let-us-know section-main">
+			<div class="wrap">
+				<span class="dr-curry"></span>
+
+				<h2>Let us know how we can help.</h2>
+				<p class="copy">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nulla turpis, dignissim porttitor fermentum sed, mattis vel ligula. Etiam cursus nunc luctus, fermentum leo eget, consequat magna. Vestibulum sapien ligula, iaculis at lectus a, elementum porttitor nisl. Maecenas at quam mattis, aliquam quam dapibus, efficitur libero.</p>
+				<p class="copy"><a class="tout" href="#">Contact us</a></p>
+			</div>
+		</section>
+
+		<footer>
+			<p class="copy">Are you ready to change your life for the better?</p>
+			<a class="cta">Request an appointment</a>
+
+			<div class="divider"></div>
+
+			<div class="legal copy">
+				<span><a href="#">Terms of Use</a></span>
+				<span><a href="#">Privacy Policy</a></span>
+				<div class="copyright">&copy; Copyright 2016 Integrative Health + Sports Performance</div>
+			</div>
+		</footer>
 	</main>
 
 	<script src="dist/built/main.built.js"></script>
